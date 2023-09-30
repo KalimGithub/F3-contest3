e6db87a082fb1a923937cb7d98bbc2054d555ad4
KalimGithub <sk22kalim@gmail.com>
2023-10-01 00:55:24 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const apiKey = 'MdMioe2b6JVfkZcjKgqaFuzcq2yDEUh1kPF44yQj';
 const form = document.getElementById("search-form")
 const submitBtn = document.getElementById("submit-btn");
 const currentImageContainer = document.getElementById("current-image-container");
+const searchInput = document.getElementById("search-input");
+const searchHistory = document.getElementById("search-history");
 
 submitBtn.addEventListener("click", (event) => {
     console.log(event.target);
@@ -16,19 +19,49 @@ submitBtn.addEventListener("click", (event) => {
 getCurrentImageOfTheDay();
 
 function getImageOfTheDay(date) {
-    const apiKey = 'MdMioe2b6JVfkZcjKgqaFuzcq2yDEUh1kPF44yQj';
     fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`)
-    .then(Response => Response.json())
-    .then(data =>{
-        currentImageContainer.innerHTML = `
+        .then(Response => Response.json())
+        .then(data => {
+            currentImageContainer.innerHTML = `
         <h2>${data.title}</h2>
         <img src="${data.url}" alt="${data.title}" />
         <p>${data.explanation}</p>
         `;
-    })
-    .catch(error => console.log(error));
+            saveSearch(date);
+        })
+        .catch(error => console.log(error));
 }
 
 function getCurrentImageOfTheDay() {
-    
+    const currentDate = new Date().toISOString().split("T")[0];
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${currentDate}`)
+        .then(Response => Response.json())
+        .then(data => {
+            currentImageContainer.innerHTML = `
+        <h2>${data.title}</h2>
+        <img src="${data.url}" alt="${data.title}" />
+        <p>${data.explanation}</p>
+        `;
+            searchInput.value = currentDate;
+        })
+        .catch(error => console.log(error));
 }
+
+
+function saveSearch(date) {
+    let search = JSON.parse(localStorage.getItem('search')) || [];
+    search.push(date);
+    localStorage.setItem('search', JSON.stringify(search));
+
+    addSearchToHistory(date);
+}
+
+function addSearchToHistory(date) {
+    const searchItem = document.createElement('li');
+    searchItem.innerText = date;
+
+    searchItem.addEventListener("click", () => {
+        getImageOfTheDay(date);
+    })
+    searchHistory.appendChild(searchItem);
+};
